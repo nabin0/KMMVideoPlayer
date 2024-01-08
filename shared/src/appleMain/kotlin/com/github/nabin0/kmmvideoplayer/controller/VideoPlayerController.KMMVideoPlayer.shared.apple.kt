1857ae4d5586080e00007df21bcf00ccdff64eaf
nabin0 <nabinbhatt62@gmail.com>
2024-01-08 15:48:05 +0530

    add playlist functionality and seekbar

--- shared/src/appleMain/kotlin/com/github/nabin0/kmmvideoplayer/controller/VideoPlayerController.KMMVideoPlayer.shared.apple.kt
+++ shared/src/appleMain/kotlin/com/github/nabin0/kmmvideoplayer/controller/VideoPlayerController.KMMVideoPlayer.shared.apple.kt
@@ -15,24 +15,30 @@ import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.cValue
 import kotlinx.coroutines.flow.MutableStateFlow
 import platform.AVFoundation.AVPlayer
-import platform.AVFoundation.AVPlayerItemDidPlayToEndTimeNotification
+import platform.AVFoundation.AVPlayerItem
 import platform.AVFoundation.AVPlayerLayer
 import platform.AVFoundation.AVPlayerTimeControlStatusPlaying
 import platform.AVFoundation.addPeriodicTimeObserverForInterval
 import platform.AVFoundation.currentItem
 import platform.AVFoundation.currentTime
 import platform.AVFoundation.duration
+import platform.AVFoundation.isPlaybackLikelyToKeepUp
 import platform.AVFoundation.pause
 import platform.AVFoundation.play
+import platform.AVFoundation.playImmediatelyAtRate
 import platform.AVFoundation.removeTimeObserver
+import platform.AVFoundation.replaceCurrentItemWithPlayerItem
+import platform.AVFoundation.seekToTime
+import platform.AVFoundation.setDefaultRate
+import platform.AVFoundation.setPreferredMaximumResolution
+import platform.AVFoundation.setRate
 import platform.AVFoundation.timeControlStatus
 import platform.AVKit.AVPlayerViewController
 import platform.CoreGraphics.CGRect
+import platform.CoreGraphics.CGSizeMake
 import platform.CoreMedia.CMTime
 import platform.CoreMedia.CMTimeGetSeconds
 import platform.CoreMedia.CMTimeMakeWithSeconds
-import platform.Foundation.NSNotificationCenter
-import platform.Foundation.NSOperationQueue
 import platform.Foundation.NSURL
 import platform.QuartzCore.CATransaction
 import platform.QuartzCore.kCATransactionDisableActions
@@ -40,11 +46,17 @@ import platform.UIKit.UIView
 import platform.darwin.Float64
 import platform.darwin.NSEC_PER_SEC
 import platform.darwin.NSObject
-import kotlin.time.DurationUnit
-import kotlin.time.toDuration
 
 actual class VideoPlayerController {
     private var avPlayer: AVPlayer? = null
+
+    private var playWhenReady: Boolean? = null
+
+    private val videoList: MutableList<AVPlayerItem> = mutableListOf()
+    private var currentVideoItemIndex: Int? = null
+
+    //val url = "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8"
+    //private val avPlayerItem = AVPlayerItem(uRL = NSURL.URLWithString(url)!!)
 
     actual val mediaDuration: MutableStateFlow<Long> = MutableStateFlow(0L)
     actual val isPlaying: MutableStateFlow<Boolean> = MutableStateFlow(false)
@@ -60,34 +72,53 @@ actual class VideoPlayerController {
 
     @OptIn(ExperimentalForeignApi::class)
     private val observer: (CValue<CMTime>) -> Unit = { time: CValue<CMTime> ->
-        println("something observed")
-        // isBuffering = avPlayer?.currentItem?.isPlaybackLikelyToKeepUp() != true
+        isBuffering.value = avPlayer?.currentItem?.isPlaybackLikelyToKeepUp() != true
         isPlaying.value = avPlayer?.timeControlStatus == AVPlayerTimeControlStatusPlaying
+        /*
         val rawTime: Float64 = CMTimeGetSeconds(time)
-        val parsedTime = rawTime.toDuration(DurationUnit.SECONDS).inWholeSeconds
-        //currentTime = parsedTime
-        if (avPlayer?.currentItem != null) {
-            // val cmTime = CMTimeGetSeconds(avPlayer.currentItem!!.duration)
-            // duration = if (cmTime.isNaN()) 0 else cmTime.toDuration(DurationUnit.SECONDS).inWholeSeconds
-        }
+         val parsedTime = rawTime.toDuration(DurationUnit.SECONDS).inWholeSeconds
+          currentTime = parsedTime
+          if (avPlayer?.currentItem != null) {
+              val cmTime = CMTimeGetSeconds(avPlayer.currentItem!!.duration)
+              duration = if (cmTime.isNaN()) 0 else cmTime.toDuration(DurationUnit.SECONDS).inWholeSeconds
+          }
+         */
+
+
+//        for (track in avPlayerItem.tracks) {
+//            val a = track as AVPlayerItemTrack
+//            val asset = a.assetTrack
+//            if (asset != null) {
+//                if (asset.mediaType == AVMediaTypeVideo) {
+//
+//                }
+//            }
+//        }
+
+
     }
 
+
+//    "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+//    "https://storage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+//    "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+//    "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
+//    "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
+//    "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+//    "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
+//    "https://storage.googleapis.com/gtv-videos-bucket/sample/Sintel.jpg",
+//    "https://storage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
+//    "https://storage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
 
     @OptIn(ExperimentalForeignApi::class)
     @Composable
     actual fun BuildPlayer(onPlayerCreated: (player: Any) -> Unit) {
-        val url =
-            "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-         avPlayer = remember { AVPlayer(uRL = NSURL.URLWithString(url)!!) }
+        // val url = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
 
-       // avPlayer = AVPlayer()
-
-
-
-        if (avPlayer != null) {
-            println("avplayer is not null")
-        }
-
+//         avPlayer = remember { AVPlayer(uRL = NSURL.URLWithString(url)!!) }
+//
+        avPlayer = AVPlayer()
+        // avPlayer?.replaceCurrentItemWithPlayerItem(avPlayerItem)
 
     }
 
@@ -96,14 +127,14 @@ actual class VideoPlayerController {
     private fun startTimeObserver() {
         val interval = CMTimeMakeWithSeconds(1.0, NSEC_PER_SEC.toInt())
         timeObserver = avPlayer?.addPeriodicTimeObserverForInterval(interval, null, observer)!!
-        NSNotificationCenter.defaultCenter.addObserverForName(
-            name = AVPlayerItemDidPlayToEndTimeNotification,
-            `object` = avPlayer?.currentItem,
-            queue = NSOperationQueue.mainQueue,
-            usingBlock = {
-                // next()
-            }
-        )
+//        NSNotificationCenter.defaultCenter.addObserverForName(
+//            name = AVPlayerItemDidPlayToEndTimeNotification,
+//            `object` = avPlayer?.currentItem,
+//            queue = NSOperationQueue.mainQueue,
+//            usingBlock = {
+//                // next()
+//            }
+//        )
     }
 
 
@@ -137,7 +168,7 @@ actual class VideoPlayerController {
         avPlayerViewController.player = avPlayer
         avPlayerViewController.showsPlaybackControls = false
 
-         startTimeObserver()
+        startTimeObserver()
 
         isPlaying.value = (avPlayer?.timeControlStatus() == AVPlayerTimeControlStatusPlaying)
 
@@ -148,6 +179,12 @@ actual class VideoPlayerController {
                 // Create a UIView to hold the AVPlayerLayer
                 val playerContainer = UIView()
                 playerContainer.addSubview(avPlayerViewController.view)
+                if (playWhenReady != null && playWhenReady == true) {
+                    avPlayer?.play()
+                    avPlayerViewController.player!!.play()
+                }
+
+                isBuffering.value = true
                 // Return the playerContainer as the root UIView
                 playerContainer
             },
@@ -160,8 +197,7 @@ actual class VideoPlayerController {
                 CATransaction.commit()
             },
             update = { view ->
-                avPlayer?.play()
-                avPlayerViewController.player!!.play()
+
             },
             modifier = Modifier.fillMaxSize()
         )
@@ -208,7 +244,13 @@ actual class VideoPlayerController {
         avPlayer?.pause()
     }
 
+    @OptIn(ExperimentalForeignApi::class)
     actual fun seekTo(millis: Long) {
+        isBuffering.value = true
+        val millsToSeconds = (millis / 1000).toDouble()
+        avPlayer?.seekToTime(CMTimeMakeWithSeconds(millsToSeconds, 1)) {
+            // Handle on seek completed or failed
+        }
     }
 
     @OptIn(ExperimentalForeignApi::class)
@@ -240,32 +282,83 @@ actual class VideoPlayerController {
     }
 
     actual fun playWhenReady(boolean: Boolean) {
+        playWhenReady = boolean
     }
 
     actual fun addPlayList(listOfVideos: List<VideoItem>) {
     }
 
     actual fun setPlayList(listOfVideos: List<VideoItem>) {
+        videoList.removeAll(videoList)
+        for (videoItem in listOfVideos) {
+            val url = videoItem.videoUrl
+            val avPlayerItem = AVPlayerItem(uRL = NSURL.URLWithString(url)!!)
+            videoList.add(avPlayerItem)
+        }
+
+        // TODO: add separate fun to play media by index
+
+        avPlayer?.replaceCurrentItemWithPlayerItem(videoList[0])
+        currentVideoItemIndex = 0
+
     }
 
+
     actual fun playNextFromPlaylist() {
+        currentVideoItemIndex?.let {
+            if (it < videoList.size - 1) {
+                val nextVideoIndex = it + 1
+                currentVideoItemIndex = nextVideoIndex
+                avPlayer?.replaceCurrentItemWithPlayerItem(videoList[nextVideoIndex])
+            }
+        }
+
     }
 
     actual fun playPreviousFromPlaylist() {
+        currentVideoItemIndex?.let {
+            if (it > 0) {
+                val nextVideoIndex = it - 1
+                currentVideoItemIndex = nextVideoIndex
+                avPlayer?.replaceCurrentItemWithPlayerItem(videoList[nextVideoIndex])
+            }
+        }
     }
 
+    private var currentPlaybackSpeed = 1f
     actual fun setPlaybackSpeed(selectedPlaybackSpeed: Float) {
+        currentPlaybackSpeed = selectedPlaybackSpeed
+        avPlayer?.setDefaultRate(selectedPlaybackSpeed)
+        // avPlayer?.rate = (selectedPlaybackSpeed)
+        avPlayer?.setRate(selectedPlaybackSpeed)
+        avPlayer?.playImmediatelyAtRate(selectedPlaybackSpeed)
     }
 
     actual fun getCurrentPlaybackSpeed(): Float {
-        return 1F
+        return currentPlaybackSpeed
     }
 
     actual fun getCurrentVideoStreamingQuality(): VideoQuality {
         return VideoQuality(0, "144", 1, null)
     }
 
+    @OptIn(ExperimentalForeignApi::class)
     actual fun setSpecificVideoQuality(videoQuality: VideoQuality) {
+        val size144 = CGSizeMake(width = 144.0, height = 256.0)
+        val size240 = CGSizeMake(width = 240.0, height = 426.0)
+        val size360 = CGSizeMake(width = 360.0, height = 640.0)
+        val size480 = CGSizeMake(width = 480.0, height = 854.0)
+        val size720 = CGSizeMake(width = 720.0, height = 1280.0)
+        val size1080 = CGSizeMake(width = 1080.0, height = 1920.0)
+
+        //avPlayerItem.setPreferredPeakBitRate(1356000.0)
+        currentVideoItemIndex?.let {
+            videoList[it].setPreferredMaximumResolution(size144)
+        }
+    }
+
+    private fun getCzSizeFromVideoQuality(videoQuality: VideoQuality) {
+
     }
 
     actual fun setSpecificCC(cc: ClosedCaptionForTrackSelector) {
--- shared/src/commonMain/kotlin/com/github/nabin0/kmmvideoplayer/data/VideoQuality.kt
+++ shared/src/commonMain/kotlin/com/github/nabin0/kmmvideoplayer/data/VideoQuality.kt
@@ -4,5 +4,6 @@ data class VideoQuality(
     val index: Int,
     val value: String,
     val resolutionKey: Int,
+    val bitrate: Double? = null,
     val dataConsumption: String? = null,
 )
