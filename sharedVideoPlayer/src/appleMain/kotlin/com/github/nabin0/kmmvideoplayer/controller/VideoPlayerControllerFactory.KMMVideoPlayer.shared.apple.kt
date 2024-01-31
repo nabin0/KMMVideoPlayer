f50d57cdce698ccf9cefea90943429f6880d8b8a
nabin0 <nabinbhatt62@gmail.com>
2024-01-31 15:02:37 +0530

    removing all branches

--- sharedVideoPlayer/src/appleMain/kotlin/com/github/nabin0/kmmvideoplayer/controller/VideoPlayerControllerFactory.KMMVideoPlayer.shared.apple.kt
+++
@@ -1,7 +0,0 @@
-package com.github.nabin0.kmmvideoplayer.controller
-
-actual class VideoPlayerControllerFactory {
-    actual fun createVideoPlayer(): VideoPlayerController {
-        return VideoPlayerController()
-    }
-}
--- sharedVideoPlayer/src/commonMain/kotlin/com/github/nabin0/kmmvideoplayer/controller/VideoPlayerControllerFactory.kt
+++
@@ -1,7 +0,0 @@
-package com.github.nabin0.kmmvideoplayer.controller
-
-import androidx.compose.runtime.Composable
-
-expect class VideoPlayerControllerFactory {
-    fun createVideoPlayer(): VideoPlayerController
-}
